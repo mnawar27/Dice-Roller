5f67885168cfd06ee5e1d2ef03f64e5ab00c04de
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-11-09 09:58:36 -0500

    Done!

--- roll.py
+++ roll.py
@@ -4,4 +4,19 @@
 # roll 2 - produce two numbers 1-6 and print them
 # roll 3 - produce three numbers and print them.
 #
+import random
 
+def roll_dice(attempts= None):
+    attempts = int(input("How many times you want to roll your dice?\n"))
+
+    for i in range (attempts):
+        rand = random.randint(1, 6)
+        print("You rolled", rand)
+
+    again = input("Wanna roll again? type yes or no\n")
+    if (again =="yes"):
+        roll_dice(attempts)
+    else:
+        print("Thanks for playing!")
+
+roll_dice()
